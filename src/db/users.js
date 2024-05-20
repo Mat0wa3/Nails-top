517456052cfe690dbf3e462b7666918d77b45c57
mat0wa3 <mmateo1804@gmail.com>
2024-05-19 20:48:27 -0500

    Correciones en el responsive design, e inicio del Dashboard layout

--- src/db/users.js
+++ src/db/users.js
@@ -6,6 +6,12 @@ export const getUser = async (user, password) => {
     return result;
 }
 
+export const getUsers = async () => {
+    const users = await Users();
+    const result = await users.find({}).toArray();
+    return result;
+}
+
 export const createUser = async (user) => {
     const users = await Users();
     const result = await users.insertOne(user);
